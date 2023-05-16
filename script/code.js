22f1beb713a16fbfe71ae766bb2021283a26a6c8
Taflnne15 <taflynnepetersen@gmail.com>
2023-05-16 14:30:53 +0200

    exc.

---
+++ script/code.js
@@ -0,0 +1,11 @@
+let subjects = ['Python',' C++' , 'VB.Net' , 'Javascript' , 'Assembly'];
+// subjects.pop();
+console.log(subjects.pop())
+
+let middle = subjects[Math.round((subjects.length - 1) / 2)];
+
+console.log(middle);
+
+
+
+
